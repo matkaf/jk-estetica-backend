d36ef0fa11aa7afd4746ea37db3fa88eaf95cb4b
Matheus Kaffka <matheus.kaffka@gmail.com>
2022-02-23 09:51:06 -0300

    bug: desinstalado pacote no_tz

--- models/cliente.js
+++ models/cliente.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const Cliente = sequelize.define('Cliente', {
     nomeCompleto: DataTypes.STRING,
     sexo: DataTypes.STRING,
-    dataNascimento: DataTypes.DATE,
+    dataNascimento: DataTypes.DATEONLY,
     celular: DataTypes.STRING,
   }, {
     timestamps: false,
